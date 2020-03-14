ad90194d47cd2a07c04c6a8d0c6db818ed1e5b1d
Maxence <maxence@aircall.io>
2020-03-14 16:47:47 +0100

    💄 display service name and created_at ; fix issue inverting name/url when creating service

--- webroot/static/code.js
+++ webroot/static/code.js
@@ -1,19 +1,34 @@
-const listContainer = document.querySelector('#service-list');
+const tableContainer = document.querySelector('#service-list');
 let servicesRequest = new Request('/service');
 
 fetch(servicesRequest)
   .then(response => response.json())
   .then(serviceList => {
     serviceList.forEach(service => {
-      var li = document.createElement("li");
+      var row = document.createElement("tr");
+      var name_column = document.createElement("td");
+      var url_column = document.createElement("td");
+      var created_at_column = document.createElement("td");
+      var status_column = document.createElement("td");
+      var actions_column = document.createElement("td");
+
+      name_column.textContent = service.name;
+      url_column.textContent = service.url;
+      status_column.textContent = service.status;
+      created_at_column.textContent = service.created_at;
+
       var deleteButton = document.createElement("button");
       deleteButton.onclick = () => onDeleteButton(service.url);
       deleteButton.textContent = "🗑";
+      actions_column.appendChild(deleteButton);
 
-      li.appendChild(document.createTextNode(service.url + ': ' + service.status));
-      li.appendChild(deleteButton);
+      row.appendChild(name_column);
+      row.appendChild(url_column);
+      row.appendChild(status_column);
+      row.appendChild(created_at_column);
+      row.appendChild(actions_column);
 
-      listContainer.appendChild(li);
+      tableContainer.appendChild(row);
     });
 });
 
